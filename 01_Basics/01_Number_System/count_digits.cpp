e67e42b2e7ef13085d13f02460f58aee976df5b3
Eklovya Sharma <eklovyasharma2004@gmail.com>
2026-02-23 19:13:19 +0530

    Basics: Added Count Digits problem

---
+++ 01_Basics/01_Number_System/count_digits.cpp
@@ -0,0 +1,41 @@
+/*
+Problem: Count Digits
+Platform: Basic Maths / Striver A2Z
+Difficulty: Easy
+
+==================================================
+Complexity Analysis:
+Time Complexity: O(log₁₀ n), number of digits
+Space Complexity: O(1)
+
+Approach(Algorithm):
+1. If n is 0, the number of digits is 1.
+2. Initialize a counter as 0.
+3. Repeatedly divide n by 10.
+4. Increment the counter for each division.
+5. Stop when n becomes 0.
+6. Return the counter.
+
+==================================================
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countDigits(int n) {
+    if (n == 0) return 1;
+    int count = 0;
+    while (n != 0){
+        count++;
+        n /= 10;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << countDigits(n);
+    return 0;
+}
+
