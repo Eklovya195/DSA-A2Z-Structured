b57bc00cc28d8e78aeb6d77f493a9a7d3d74c8e4
Eklovya Sharma <eklovyasharma2004@gmail.com>
2026-04-08 15:56:49 +0530

    Updated some Problems

--- 04_BinarySearch/1_Binary_Search_1D_Array/03_Upper_Bound.cpp
+++ 04_BinarySearch/1_Binary_Search_1D_Array/03_Upper_Bound.cpp
@@ -4,10 +4,10 @@ Platform: Binary Search / Striver A2Z
 Difficulty: Easy
 Practice Link: "https://www.geeksforgeeks.org/problems/implement-upper-bound/1"
 ==================================================
-Input:  arr[] = [2, 3, 7, 10, 11, 11, 25], target = 9
+Input: arr[] = [2, 3, 7, 10, 11, 11, 25], target = 9
 Output: 3
-Explanation: 3 is the smallest index in arr[] 
-where element (arr[3] = 10) is greater than or equal to 9.
+Explanation: 3 is the smallest index in arr[],
+at which element (arr[3] = 10) is larger than 9.
 ==================================================
 */
 
@@ -16,7 +16,7 @@ where element (arr[3] = 10) is greater than or equal to 9.
 #include <bits/stdc++.h>
 using namespace std;
 
-int LowerBound(vector<int> &arr,int n,int target){
+int UpperBound(vector<int> &arr,int n,int target){
     for(int i=0;i<n;i++){
         if(arr[i] > target){
             return i;
@@ -29,7 +29,7 @@ int main(){
     vector<int> arr = {2, 3, 7, 10, 11, 11, 25};
     int n = arr.size();
     int target = 11;
-    int ans = LowerBound(arr,n,target);
+    int ans = UpperBound(arr,n,target);
     cout << ans;
     return 0;
 }
@@ -48,7 +48,7 @@ Space Complexity: 0(1), no extra space being used
 #include <bits/stdc++.h>
 using namespace std;
 
-int LowerBound(vector<int> &arr,int n,int target){
+int UpperBound(vector<int> &arr,int n,int target){
     int low = 0;
     int high = n-1;
     int ans = n;
@@ -69,7 +69,7 @@ int main(){
     vector<int> arr = {2, 3, 7, 10, 11, 11, 25};
     int n = arr.size();
     int target = 11;
-    int ans = LowerBound(arr,n,target);
+    int ans = UpperBound(arr,n,target);
     cout << ans;
     return 0;
 }
